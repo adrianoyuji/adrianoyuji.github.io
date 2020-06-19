a11580fb76a684d20f81041f7d30098d8d949642
adrianoyuji <adrianoyuji@gmail.com>
2020-06-19 18:39:53 -0400

    Updates

---
+++ precache-manifest.dba0e670019114189bcc00ec8355260f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef3a29d674e23f9848e6110f58f7009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d95ff55f76d5ef86f14a",
+    "url": "/static/css/main.d972ae97.chunk.css"
+  },
+  {
+    "revision": "a602292ad45e9261b6f6",
+    "url": "/static/js/2.2e23814a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2e23814a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d95ff55f76d5ef86f14a",
+    "url": "/static/js/main.c070c080.chunk.js"
+  },
+  {
+    "revision": "cf98464fe224a6937ffd",
+    "url": "/static/js/runtime-main.5932411e.js"
+  },
+  {
+    "revision": "2ab834f19851e8969cd6b87b7f2bcfac",
+    "url": "/static/media/background.2ab834f1.jpg"
+  }
+]);
